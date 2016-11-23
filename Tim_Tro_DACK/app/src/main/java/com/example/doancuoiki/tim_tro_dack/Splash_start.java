90fb460c06fc6edb36a5de5df817c1babe8da19c
AnhTai1995 <anhtai.hcmus95@gmail.com>
2016-11-23 14:47:12 +0700

    Tạo source cấu trúc ban đầu của ứng dụng + chức năng tìm kiếm địa điểm bằng bản đồ

---
+++ Tim_Tro_DACK/app/src/main/java/com/example/doancuoiki/tim_tro_dack/Splash_start.java
@@ -0,0 +1,49 @@
+package com.example.doancuoiki.tim_tro_dack;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class Splash_start extends AppCompatActivity {
+
+    private ProgressBar mProgress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        // Show the splash screen
+        setContentView(R.layout.activity_splash_start);
+        mProgress = (ProgressBar) findViewById(R.id.splashprogressBar);
+        mProgress.getIndeterminateDrawable().setColorFilter(Color.parseColor("#A9F5F2"), PorterDuff.Mode.MULTIPLY);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(200);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                //  Timber.e(e.getMessage());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(Splash_start.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Tim_Tro_DACK/app/src/main/java/com/example/doancuoiki/tim_tro_dack/demo.java
@@ -0,0 +1,20 @@
+package com.example.doancuoiki.tim_tro_dack;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class demo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_demo,null);
+    }
+}
