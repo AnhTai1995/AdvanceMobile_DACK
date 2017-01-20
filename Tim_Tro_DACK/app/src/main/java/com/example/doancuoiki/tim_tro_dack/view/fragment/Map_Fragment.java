a1b1496c73cdc8000ac95bea5ca2a0eb24ec5616
NguyenQuoc <nguyenx2quoc@gmail.com>
2017-01-20 08:53:28 +0700

    Update file Map_Fragment.java
    Signed-off-by: NguyenQuoc <nguyenx2quoc@gmail.com>

--- Tim_Tro_DACK/app/src/main/java/com/example/doancuoiki/tim_tro_dack/view/fragment/Map_Fragment.java
+++ Tim_Tro_DACK/app/src/main/java/com/example/doancuoiki/tim_tro_dack/view/fragment/Map_Fragment.java
@@ -9,10 +9,10 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.doancuoiki.tim_tro_dack.R;
 import com.example.doancuoiki.tim_tro_dack.apihelper.Service;
-import com.example.doancuoiki.tim_tro_dack.model.KinhDoViDo;
 import com.example.doancuoiki.tim_tro_dack.model.Tro;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,7 +21,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -33,8 +32,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Map_Fragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
-    private ArrayList<KinhDoViDo> lt;
     private  List<Tro> nhaTrot;
+    private LatLng latlng;
+    private Button bttimkiem;
 
     @Nullable
     @Override
@@ -46,43 +46,55 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
         mapFragment.getMapAsync(this);
 
+        bttimkiem= (Button)v.findViewById(R.id.bttimvung);
+        bttimkiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("http://renthouseapi.apphb.com/api/v1/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                //Service service = retrofit.create(Service.class);
+                Service service = retrofit.create(Service.class);
+                //APIService apiService = retrofit.create(APIService.class);
+
+                Call<List<Tro>> call = service.getTatCaNhaTro();
+                call.enqueue(new Callback<List<Tro>>() {
+                    @Override
+                    public void onResponse(Call<List<Tro>> call, Response<List<Tro>> response) {
+                        nhaTrot = response.body();
+                        //Chạy for
+                        for(int i=0; i < nhaTrot.size(); i++){
+                            if(nhaTrot.get(i).getKinhDo() != null && nhaTrot.get(i).getViDo() != null)
+                            {
+                                Double kd = Double.parseDouble(nhaTrot.get(i).getKinhDo());
+                                Double vd = Double.parseDouble(nhaTrot.get(i).getViDo());
+                                LatLng latlng1 = new LatLng(vd,kd);
+                                mMap.addMarker(new MarkerOptions().position(latlng1).title(nhaTrot.get(i).getTinhTrang()));
+                                //     mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+                                LatLng HCM = new LatLng(10.7626391,106.6820268);
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HCM, 13));
+
+                            }
+
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<List<Tro>> call, Throwable t) {
+                        //Log.e(TAG, t.getMessage());
+                    }
+                });
+            }
+        });
+
         return v;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://renthouseapi.apphb.com/api/v1/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        //Service service = retrofit.create(Service.class);
-        Service service = retrofit.create(Service.class);
-        //APIService apiService = retrofit.create(APIService.class);
 
-        Call<List<Tro>> call = service.getTatCaNhaTro();
-        call.enqueue(new Callback<List<Tro>>() {
-            @Override
-            public void onResponse(Call<List<Tro>> call, Response<List<Tro>> response) {
-                nhaTrot = response.body();
-                //Chạy for
-                for(int i=0; i < nhaTrot.size(); i++){
-                    //Get map gì của mày đó
-//                    nhaTrot.get(i).getKinhDo();
-//                    nhaTrot.get(i).getViDo();
-                }
-            }
-            @Override
-            public void onFailure(Call<List<Tro>> call, Throwable t) {
-                //Log.e(TAG, t.getMessage());
-            }
-        });
-
-        LatLng HCM = new LatLng(10.771944,106.698056);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HCM, 13));
-        // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(HCM).title("Marker in HCM"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(HCM));
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
